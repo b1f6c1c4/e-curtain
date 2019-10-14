a158e4c1cb5d2355d4973844982ae6535bcc2692
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-10-14 03:34:15 -0400

    motor

---
+++ left.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+p = GPIO.PWM(7, 1 / 3000e-6)
+p.start(75)
+sleep(10.0)
+p.stop()
+
+GPIO.output(7, GPIO.LOW)
+
+GPIO.cleanup()
