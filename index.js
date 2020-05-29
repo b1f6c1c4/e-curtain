47a7f31b0c739236a8cc24e9bb8bcacfd2938270
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-05-29 02:31:42 -0400

    daily

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dayjs = require('dayjs');
 const { spawn } = require('child_process');
+const isoWeek = require('dayjs/plugin/isoWeek')
+dayjs.extend(isoWeek);
 const app = express();
 
 app.use(express.static(__dirname));
@@ -22,27 +24,68 @@ const goLeft = () => {
   }
 };
 
+const getTime = (str) => {
+  const tn = dayjs();
+  let tx = dayjs(tn.format('YYYY-MM-DDT') + str, 'YYYY-MM-DDTHH:mm', true);
+  if (tx.isBefore(tn)) {
+    tx = tx.add(1, 'day');
+  }
+  return tx;
+};
+
 function getDelay(req, res, next) {
   if (req.body === 'now') {
     req.delay = 0;
   } else if (req.body.startsWith('+')) {
     req.delay = +req.body;
   } else {
-    const tn = dayjs();
-    let tx = dayjs(tn.format('YYYY-MM-DDT') + req.body, 'YYYY-MM-DDTHH:mm', true);
-    if (tx.isBefore(tn)) {
-      tx = tx.add(1, 'day');
-    }
-    req.delay = tx.diff(tn, 's') / 60;
+    req.delay = getTime(req.body).diff(dayjs(), 's') / 60;
   }
   next();
 }
 
+let tomorrowMorning = null;
+let morning = null;
 const pending = [];
 
 app.post('/open', bodyParser.text(), getDelay, (req, res) => {
-  pending.push(setTimeout(goRight, req.delay * 60 * 1000));
-  res.send(`opening ${req.delay} mins later`);
+  if (req.body === '?') {
+    if (!tomorrowMorning) {
+      res.send('tomorrow no morning open');
+    } else {
+      delay = tomorrowMorning.diff(dayjs(), 's') / 60;
+      res.send(`tomorrow will open ${tomorrowMorning.format('HH:mm')}, nearest of which is ${delay} mins later (${tomorrowMorning.format('YYYY-MM-DDTHH:mm (ddd)')})`);
+    }
+  } else if (req.body === '??') {
+    if (!morning) {
+      res.send('no morning open');
+    } else {
+      delay = morning.diff(dayjs(), 's') / 60;
+      res.send(`open every ${morning.format('HH:mm')}, nearest of which is ${morning.format('YYYY-MM-DDTHH:mm (ddd)')}`);
+    }
+  } else if (req.body === '==0') {
+    tomorrowMorning = morning = null;
+    res.send('no more morning open');
+  } else if (req.body === '=0') {
+    tomorrowMorning = null;
+    res.send('tomorrow no morning open');
+  } else if (req.body.startsWith('==')) {
+    morning = tomorrowMorning = getTime(req.body.substr(2));
+    if (morning.isoWeekday() <= 4) {
+      morning = morning.add(1, 'day');
+    } else {
+      morning = morning.add(8 - morning.isoWeekday(), 'day');
+    }
+    delay = tomorrowMorning.diff(dayjs(), 's') / 60;
+    res.send(`open every ${tomorrowMorning.format('HH:mm')}, nearest of which is ${delay} mins later (${tomorrowMorning.format('YYYY-MM-DDTHH:mm (ddd)')})`);
+  } else if (req.body.startsWith('=')) {
+    tomorrowMorning = getTime(req.body.substr(1));
+    delay = tomorrowMorning.diff(dayjs(), 's') / 60;
+    res.send(`tomorrow will open ${tomorrowMorning.format('HH:mm')}, nearest of which is ${delay} mins later`);
+  } else {
+    pending.push(setTimeout(goRight, req.delay * 60 * 1000));
+    res.send(`opening ${req.delay} mins later`);
+  }
 });
 
 app.post('/close', bodyParser.text(), getDelay, (req, res) => {
@@ -55,6 +98,27 @@ app.post('/cancel', (req, res) => {
   pending.length = 0;
   res.send('cancelled');
 });
+
+setInterval(() => {
+  if (tomorrowMorning) {
+    if (dayjs().isAfter(tomorrowMorning)) {
+      goRight();
+      tomorrowMorning = null;
+    }
+  }
+  if (tomorrowMorning || !morning) return;
+  const d = morning.diff(dayjs(), 'd');
+  if (d < 1.2) {
+    tomorrowMorning = morning;
+    if (morning.isoWeekday() <= 4) {
+      morning = morning.add(1, 'day');
+    } else {
+      morning = morning.add(8 - morning.isoWeekday(), 'day');
+    }
+    console.log(`tmr=${tomorrowMorning.format('YYYY-MM-DDTHH:mm')}`);
+    console.log(`mr=${morning.format('YYYY-MM-DDTHH:mm')}`);
+  }
+}, 1000);
 
 const port = process.env.DEBUG ? 3000 : 80;
 console.log(`Starting httpd server on 0.0.0.0:${port}`)
