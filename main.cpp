2f64ae4a749eff956243626cf75023f66f0864eb
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-09-29 17:51:03 -0400

    timer

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include "common.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
 #include "si7021.hpp"
 #include "filter.hpp"
 #include "udp_client.hpp"
@@ -8,9 +11,16 @@ int main() {
     std::array<lp_filter, 2> lps;
     std::array<df_filter, 2> dfs;
     udp_client<4> udp{ "192.168.1.66", 23333 };
+
+    using namespace std::chrono_literals;
+    auto dt = 500ms; // 2Hz
+    auto clk{ std::chrono::system_clock::now() };
+
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
     while (true) {
+        clk += dt;
+
         arr_t<2> tr0, tr, dtr;
         sensor >> tr0;
 
@@ -18,6 +28,16 @@ int main() {
         tr0 >> dfs >> dtr;
 
         udp << std::array{ tr[0], tr[1], dtr[0], dtr[1] };
+
+        auto aft{ std::chrono::system_clock::now() };
+        auto diff{ clk - aft };
+        if (diff > 1.05 * dt || diff < 0.95 * dt) { // abnormal skew, reset
+            clk = aft;
+            std::cerr << "Warning: abnormal clock skew" << std::endl;
+            std::this_thread::sleep_for(dt);
+        } else {
+            std::this_thread::sleep_for(diff);
+        }
     }
 #pragma clang diagnostic pop
 }
