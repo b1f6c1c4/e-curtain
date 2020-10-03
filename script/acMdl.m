0ddea63ae3a7d979bc8b01da4b3ed425909666fd
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-10-03 19:50:19 -0400

    mdl: fix Wx

--- script/acMdl.m
+++ script/acMdl.m
@@ -1,7 +1,7 @@
 %% The continuous-time model
 
 % Assumptions:
-W01ass = 10; % t1-t0 = 10
+t1m0ass = 10; % t1-t0 = 10
 
 % [ts1 ts2 td1 td2] -> d/dt [ts1;ts2;td1;td2]
 A0 = [
@@ -23,15 +23,15 @@ B2 = [
     -1 1 -1 0 0
     0 0 0 0 1
     ];
-% [ac1 ac2 f012 t0d] -> [Wac1 Wac2 W01 W012 Wx]
+% [ac1 ac2 f012 t0d w0 w1 w2] -> [W1 W2 W01 W012 Wx]
 B1 = [
-    2500 0 0 0
-    0 2500 0 0
-    0 0 -350*W01ass 0
-    0 0 0 155
-    0 0 -1000 0
+    2500 0 0 0 0 200 0
+    0 2500 0 0 1100 0 300
+    0 0 -350*t1m0ass 0 0 0 0
+    0 0 0 155 0 0 0
+    0 0 -140*t1m0ass 0 0 0 0
     ];
-% [ts td] -> [Wac1 Wac2 W01 W012 Wx]
+% [ts td] -> [W1 W2 W01 W012 Wx]
 BA = [
     0 0
     0 0
@@ -49,17 +49,20 @@ C1 = [
     0.25 0.75
     -1 1
     ];
-% [ac1 ac2 f012 t0d] -> [t1 t2]
+% [ac1 ac2 f012 t0d w0 w1 w2] -> [t1 t2]
 D0 = [
-    0 0 0 0
-    0 0 0 0
+    0 0 0 0 0 0 0
+    0 0 0 0 0 0 0
     ];
-% [ac acd f012 t0d] -> [ac1 ac2 f012 t0d]
+% [ac acd f012 t0d w0 w1 w2] -> [ac1 ac2 f012 t0d w0 w1 w2]
 X = [
-    0.5 -0.5 0 0
-    0.5 0.5 0 0
-    0 0 1 0
-    0 0 0 1
+    0.5 -0.5 0 0 0 0 0
+    0.5 0.5 0 0 0 0 0
+    0 0 1 0 0 0 0
+    0 0 0 1 0 0 0
+    0 0 0 0 1 0 0
+    0 0 0 0 0 1 0
+    0 0 0 0 0 0 1
     ];
 
 A = A0 + B3*B2*BA*C2;
@@ -68,7 +71,8 @@ C = C1\C2;
 D = D0; % *X
 
 G = ss(A, B, C, D, ...
-    'InputName', {'ac1', 'ac2', 'f012', 't0d'}, 'OutputName', {'t1', 't2'});
+    'InputName', {'ac1', 'ac2', 'f012', 't0d', 'w0', 'w1', 'w2'}, ...
+    'OutputName', {'t1', 't2'});
 
 %% The t0->t0d filter
 
@@ -90,49 +94,71 @@ t0filts = ss(fA, fB, fC, fD, 1/Fs);
 Ts = 30;
 
 Gd = c2d(G, Ts);
-Gd = setmpcsignals(Gd, 'MV', [1 2 3], 'MD', 4);
-m = mpc(Gd, Ts, 5, [], struct(...
-    'ManipulatedVariables', [0.1 0.1 0], ...
+Gd = setmpcsignals(Gd, 'MV', [1 2 3], 'MD', [4 5 6 7]);
+m = mpc(Gd, Ts, 5, 2, struct(...
+    'ManipulatedVariables', [0.1 0.1 0.002], ...
     'ManipulatedVariablesRate', [0 0 0.02], ...
-    'OutputVariables', [1 100]));
+    'OutputVariables', [3 1]));
 conE = [
     1 1 0
     -1 -1 0
     1 -1 0
     -1 1 0
+    1 0 0
+    -1 0 0
+    0 1 0
+    0 -1 0
     0 0 1
     0 0 -1
     ];
-conG = [2; 2; 2; 2; 1; 0];
-conV = [0; 0; 0; 0; 0; 0];
+conG = [2; 2; 2; 2; 1.8; 1.8; 1.2; 1.2; 1; 0];
+conV = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
 setconstraint(m, conE, [], conG, conV);
 
 %%
 
 [configData,stateData,onlineData] = getCodeGenerationData(m);
 
-% yCodeGen = [];
-% uCodeGen = [];
-% x = [0;0;0;0];
-% T = 1000;
-%     
-% for ct = 1:floor(T/Ts)
-%     % Update and store plant output.
-%     y = Gd.C*x;
-%     yCodeGen = [yCodeGen y];
-%     % Update measured output in online data.
-%     onlineData.signals.ym = y;
-%     onlineData.signals.md = 0;
-%     % Update reference in online data.
-%     onlineData.signals.ref = [-1 0];
-%     %onlineData.weights.y = [0.1 2];
-%     % Update constraints in online data.
-%     % Compute control actions.
-%     [u,statedata] = mpcmoveCodeGeneration(configData,stateData,onlineData);
-%     % Update and store plant state.
-%     x = Gd.A*x + Gd.B*[u;0];
-%     uCodeGen = [uCodeGen u];
-% end
+yCodeGen = [];
+uCodeGen = [];
+x = [0;0;0;0];
+T = 1000;
+    
+for ct = 1:floor(T/Ts)
+    % Update and store plant output.
+    y = Gd.C*x;
+    yCodeGen = [yCodeGen y];
+    % Update measured output in online data.
+    onlineData.signals.ym = y;
+    onlineData.signals.md = 0;
+    % Update reference in online data.
+    onlineData.signals.ref = [0 -2];
+    onlineData.weights.y = [1 3];
+    % Update constraints in online data.
+    % Compute control actions.
+    [u,statedata] = mpcmoveCodeGeneration(configData,stateData,onlineData);
+    % Update and store plant state.
+    ac1q = u(1); ac2q = u(2);
+    if ac1q*ac2q <= 0
+        if abs(ac1q) > ac2q
+            ac2q = 0;
+        else
+            ac1q = 0;
+        end
+    else
+        acq = ac1q + ac2q;
+        if ac1q / ac2q < 0.4/0.6
+            ac1q = acq * 0.4;
+            ac2q = acq * 0.6;
+        elseif ac1q / ac2q > 0.9/0.1
+            ac1q = acq * 0.9;
+            ac2q = acq * 0.1;
+        end
+    end
+    u = [ac1q;ac2q;u(3)];
+    x = Gd.A*x + Gd.B*[u;-10;1;0;3];
+    uCodeGen = [uCodeGen u];
+end
 
 onlineData.weights.y = [1 1];
 onlineData.limits.umax = [1;1;1];
@@ -141,19 +167,19 @@ func = 'mpcmoveCodeGeneration';
 funcOutput = 'dumbac';
 Cfg = coder.config('lib');
 Cfg.DynamicMemoryAllocation = 'off';
-codegen('-d', 'libdumbac', '-c', '-config',Cfg,func,'-o',funcOutput,'-args',...
-    {coder.Constant(configData),stateData,onlineData});
+%codegen('-d', 'libdumbac', '-c', '-config',Cfg,func,'-o',funcOutput,'-args',...
+%    {coder.Constant(configData),stateData,onlineData});
 
 %%
 
-sim(m, T/Ts, [-1 0], 0);
+sim(m, T/Ts, [-1 0], [-10 1 0 0]);
 
 %%
 
-figure(3);
-subplot(2,1,1);
-plot(yCodeGen.');
-grid on;
-subplot(2,1,2);
-grid on;
-plot(uCodeGen.');
+% figure(3);
+% subplot(2,1,1);
+% plot((0:ct-1).*Ts, yCodeGen.');
+% grid on;
+% subplot(2,1,2);
+% grid on;
+% plot((0:ct-1).*Ts, uCodeGen.');
