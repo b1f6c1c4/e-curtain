6c32d36db7bf7c188b2c90c876aa0485a8593272
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-10-07 18:56:53 -0400

    WIP: ac ak1 ak2

--- script/acMdl.m
+++ script/acMdl.m
@@ -1,36 +1,124 @@
+%% The LPV matrixes
+
+kt = 1250/(2100*8e-6/1e-3*0.5);
+
+% [ts1 ts2 tac td1 td2] -> d/dt [ts1;ts2;tac;td1;td2]
+A0 = [
+    0   0     1e-4  0       0
+    0   -4e-3 0     0       0
+    0   0     -1e-3 0       0
+    0   0     0     -0.4e-3 0
+    0   0     0     0       -7e-3
+    ];
+% [Wac W1+W2+W01+W012 W2-W1-W01 Wx] -> d/dt [ts1;ts2;tac;td1;td2]
+B3 = [
+    0    0.14e-6 0       0
+    0    0       0       2e-6
+    4e-6 0       0       0
+    0    0       0.38e-6 0
+    0    0       0       -10e-6
+    ];
+% [Wac W1 W2 W01 W012 Wx] -> [Wac W1+W2+W01+W012 W2-W1-W01 Wx]
+B2 = [
+    1  0  0  0  0  0
+    0  +1 +1 +1 +1 0
+    0  -1 +1 -1 0  0
+    0  0  0  0  0  +1
+    ];
+% [ac ak1 ak2 f012 cur t0d w0 w1 w2] -> [Wac W1 W2 W01 W012 Wx]
+%   1  tac tac t1m0       Wsun-50  1   1    0   0
+B1s = [
+    2100 0  0  0          0        0   0    0   0
+    200  kt 0  0          0        0   0    200 0
+    200  0  kt 0          0        0   1100 0   300
+    0    0  0  -350       1        0   0    0   0
+    0    0  0  0          0        155 0    0   0
+    0    0  0  -140       0        0   0    0   0
+    ];
+Blpv = [
+    0 0 0 1
+    0 0 1 0
+    0 0 1 0
+    1 0 0 0
+    0 1 0 0
+    0 0 0 1
+    0 0 0 1
+    0 0 0 1
+    0 0 0 1
+    ];
+% [ts td tac] -> [Wac W1 W2 W01 W012 Wx]
+BA = [
+    0 0 0
+    0 0 0
+    0 0 0
+    0 0 0
+    -155 0 0
+    0 0 0
+    ];
+% [ts1 ts2 tac td1 td2] -> [ts td tac]
+C2 = [
+    1 1 0 0 0
+    0 0 0 1 1
+    0 0 1 0 0
+    ];
+% [t1 t2 tac] -> [ts td tac]
+C1 = [
+    0.25 0.75 1
+    -1 1 1
+    0 0 1
+    ];
+% [ac ak1 ak2 f012 cur t0d w0 w1 w2] -> [t1 t2]
+D0 = [
+    0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0
+    0 0 0 0 0 0 0 0 0
+    ];
+
+fmakeACMdl = @(t1m0, Wsun, tac, Ts) makeACMdl(t1m0, Wsun, tac, Ts, ...
+    A0, B3, B2, B1s, Blpv, BA, C2, C1, D0);
+
 %% The LPV model
 
 Ts = 10;
 t1m0s = [-20 40];
 Wsuns = [0 1600];
-Gds = drss(4,2,8,length(t1m0s),length(Wsuns));
+tacs = [-20 20];
+Gds = drss(5,3,9,length(t1m0s),length(Wsuns),length(tacs));
 for i = 1:length(t1m0s)
     for j = 1:length(Wsuns)
-        Gds(:,:,i,j) = makeACMdl(t1m0s(i), Wsuns(j), Ts);
+        for k = 1:length(tacs)
+            Gds(:,:,i,j,k) = fmakeACMdl(t1m0s(i), Wsuns(j), tacs(k), Ts);
+        end
     end
 end
-[t1m0sg,Wsunsg] = ndgrid(t1m0s, Wsuns);
-Gds.SamplingGrid = struct('t1m0', t1m0sg, 'Wsun', Wsunsg);
+[t1m0sg,Wsunsg,tacsg] = ndgrid(t1m0s, Wsuns, tacs);
+Gds.SamplingGrid = struct('t1m0', t1m0sg, 'Wsun', Wsunsg, 'tac', tacsg);
 
-Gdsam = mpc(makeACMdl(10, 60, Ts), Ts, 10, 2, struct(...
-    'ManipulatedVariables', [0.1 0.1 0.002 0], ...
-    'ManipulatedVariablesRate', [0 0 0.02 0.01]));
+Gdsam = mpc(fmakeACMdl(10, 60, -16, Ts), Ts, 10, 2, struct(...
+    'ManipulatedVariables',     [0    0.001 0.001 0.002 0    ], ...
+    'ManipulatedVariablesRate', [0    0     0     0.015 0.012]));
 conE = [
-    1 1 0 0
-    -1 -1 0 0
-    1 -1 0 0
-    -1 1 0 0
-    1 0 0 0
-    -1 0 0 0
-    0 1 0 0
-    0 -1 0 0
-    0 0 -1 0
-    0 0 1 0
-    0 0 0 -1
-    0 0 0 1
+    -1 0  0  0  0
+    +1 0  0  0  0
+    0  -1 0  0  0
+    0  +1 0  0  0
+    0  0  -1 0  0
+    0  0  +1 0  0
+    0  +1 +1 0  0
+    0  0  0  -1 0
+    0  0  0  +1 0
+    0  0  0  0  -1
+    0  0  0  0  +1
     ];
-conG = [2; 2; 2; 2; 1.8; 1.8; 1.2; 1.2; 0; 1; 0; 1];
-conV = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
+conG = [
+    2; 2
+    -(0.4-0.08); 0.9-0.08
+    -(0.1-0.08); 0.6-0.08
+    1
+    0; 1
+    0; 1
+    ];
+conV = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0; 0];
 setconstraint(Gdsam, conE, [], conG, conV);
 
 %% The t->t filter
--- script/makeACMdl.m
+++ script/makeACMdl.m
@@ -1,78 +1,17 @@
-function [Gd]=makeACMdl(t1m0, Wsun, Ts)
+function [Gd]=makeACMdl(t1m0, Wsun, tac, Ts, A0, B3, B2, B1s, Blpv, BA, C2, C1, D0)
 
-% [ts1 ts2 td1 td2] -> d/dt [ts1;ts2;td1;td2]
-A0 = [
-    0 0 0 0
-    0 -4e-3 0 0
-    0 0 -0.4e-3 0
-    0 0 0 -7e-3
-    ];
-% [W1+W2+W01+W012 W2-W1-W01 Wx] -> d/dt [ts1;ts2;td1;td2]
-B3 = [
-    0.14e-6 0 0
-    0 0 2e-6
-    0 0.38e-6 0
-    0 0 -10e-6
-    ];
-% [W1 W2 W01 W012 Wx] -> [W1+W2+W01+W012 W2-W1-W01 Wx]
-B2 = [
-    1 1 1 1 0
-    -1 1 -1 0 0
-    0 0 0 0 1
-    ];
-% [ac1 ac2 f012 cur t0d w0 w1 w2] -> [W1 W2 W01 W012 Wx]
-B1 = [
-    2500 0    0          0          0   0    200 0
-    0    2500 0          0          0   1100 0 300
-    0    0  -350*t1m0    Wsun-50    0   0    0 0
-    0    0    0          0          155 0    0 0
-    0    0  -140*t1m0    0          0   0    0 0
-    ];
-% [ts td] -> [W1 W2 W01 W012 Wx]
-BA = [
-    0 0
-    0 0
-    0 0
-    -155 0
-    0 0
-    ];
-% [ts1 ts2 td1 td2] -> [ts td]
-C2 = [
-    1 1 0 0
-    0 0 1 1
-    ];
-% [t1 t2] -> [ts td]
-C1 = [
-    0.25 0.75
-    -1 1
-    ];
-% [ac1 ac2 f012 cur t0d w0 w1 w2] -> [t1 t2]
-D0 = [
-    0 0 0 0 0 0 0 0
-    0 0 0 0 0 0 0 0
-    ];
-% [ac acd f012 cur t0d w0 w1 w2] -> [ac1 ac2 f012 cur t0d w0 w1 w2]
-% X = [
-%     0.5 -0.5 0 0 0 0 0 0
-%     0.5 0.5 0 0 0 0 0 0
-%     0 0 1 0 0 0 0 0
-%     0 0 0 1 0 0 0 0
-%     0 0 0 0 1 0 0 0
-%     0 0 0 0 0 1 0 0
-%     0 0 0 0 0 0 1 0
-%     0 0 0 0 0 0 0 1
-%     ];
+B1 = B1s .* ([t1m0 Wsun tac 1] * Blpv.');
 
 A = A0 + B3*B2*BA*C2;
-B = B3*B2*B1; % *X
+B = B3*B2*B1;
 C = C1\C2;
-D = D0; % *X
+D = D0;
 
 G = ss(A, B, C, D, ...
-    'InputName', {'ac1', 'ac2', 'f012', 'cur', 't0d', 'w0', 'w1', 'w2'}, ...
-    'OutputName', {'t1', 't2'});
+    'InputName', {'ac', 'ak1', 'ak2', 'f012', 'cur', 't0d', 'w0', 'w1', 'w2'}, ...
+    'OutputName', {'t1', 't2', 'tac'});
 
 Gd = c2d(G, Ts);
-Gd = setmpcsignals(Gd, 'MV', [1 2 3 4], 'MD', [5 6 7 8]);
+Gd = setmpcsignals(Gd, 'MV', [1 2 3 4 5], 'MD', [6 7 8 9], 'MO', [1 2], 'UO', 3);
 
 end
