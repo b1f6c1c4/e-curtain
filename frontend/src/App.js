1fc13d218075bb1bd99c96845720e469fd239f3d
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-10-08 19:16:21 -0400

    grommet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Box, Button, Collapsible, Heading, Grommet } from 'grommet';
+import { Notification } from 'grommet-icons';
 
-function App() {
+const theme = {
+  global: {
+    colors: {
+      brand: '#228be6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+const App = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} themeMode="dark" full>
+      <Box fill>
+        <AppBar>
+          <Heading level='3' margin='none'>My App</Heading>
+          <Button
+            icon={<Notification />}
+            onClick={() => { setShowSidebar(!showSidebar) }}
+          />
+        </AppBar>
+        <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+          <Box flex align='center' justify='center'>
+            app body
+          </Box>
+          <Collapsible direction="horizontal" open={showSidebar}>
+            <Box
+              width='medium'
+              background='light-2'
+              evelation='small'
+              align='center'
+              justify='center'
+            >
+              sidebar
+            </Box>
+          </Collapsible>
+        </Box>
+      </Box>
+    </Grommet>
   );
 }
 
