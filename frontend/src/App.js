a633b3b74844cf0460b273c215481cb6e520129f
b1f6c1c4 <b1f6c1c4@gmail.com>
2020-10-09 22:45:07 -0400

    api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,8 @@
 import React, { useState } from 'react';
-import {
-  Box,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
-  DataChart,
-  Grommet,
-  RangeSelector,
-  ResponsiveContext,
-  Text,
-} from 'grommet';
-import { IconButton } from 'grommet-controls';
-import { Add, Radial, Subtract } from 'grommet-icons';
-import GaugeChart from 'react-gauge-chart';
-import dayjs from 'dayjs';
+import { Grommet } from 'grommet';
+import ky from 'ky';
+import Dashboard from './Dashboard';
+import { useInterval } from '@react-corekit/use-interval';
 
 const theme = {
   global: {
@@ -35,238 +23,35 @@ const theme = {
   },
 };
 
-const Room = (props) => {
-  const size = React.useContext(ResponsiveContext);
-  const isSmall = size === 'small' || size === 'xsmall';
-  const { id, nid, title } = props;
+const App = () => {
+  const [current, setCurrent] = useState(undefined);
+  const [history, setHistory] = useState(undefined);
+
+  useInterval(async () => {
+    try {
+      setCurrent((await ky.get('/current')).json());
+    } catch (e) {
+      console.error(e);
+      setCurrent(undefined);
+    }
+  }, 5000);
+  useInterval(async () => {
+    try {
+      setHistory((await ky.get('/history')).json());
+    } catch (e) {
+      console.error(e);
+      setCurrent(undefined);
+    }
+  }, 5000);
+
   return (
-    <Card width={{ min: '100px' }} background='light' margin='small' flex>
-      <CardHeader pad='small' justify='center' background={id}>{title}</CardHeader>
-      <CardBody pad={{ top: 'small' }} flex='grow' align='center'>
-        <GaugeChart id={'gauge-' + id + '-t'}
-          colors={['#8cb964', '#fae500', '#ca4951']}
-          arcsLength={[0.4, 0.1, 0.4]}
-          arcWidth={0.5}
-          percent={0.37}
-          hideText
-          animate={false}
-        />
-        <Box>
-          <Text weight='bold'>t<sub>{nid}</sub>: 37.11&#8451;</Text>
-        </Box>
-        <Box flex='grow'>
-          {nid !== '0' ? (
-            <Text>t<sub>p{nid}</sub>: 37.11&#8451;</Text>
-          ) : (
-            <Box direction='row' gap='small'>
-              <Text>4.11</Text>
-              <Text>3m/s</Text>
-            </Box>
-          )}
-        </Box>
-        {!isSmall && (
-          <GaugeChart id={'gauge-' + id + '-h'}
-            colors={['#8cb964', '#fae500', '#ca4951']}
-            arcsLength={[0.3, 0.6, 0.1]}
-            arcWidth={0.3}
-            percent={0.37}
-            hideText
-            animate={false}
-          />
-        )}
-        <Box>
-          <Text weight='bold'>h<sub>{nid}</sub>: 65.43%</Text>
-        </Box>
-      </CardBody>
-    </Card>
+    <Grommet theme={theme} full>
+      <Dashboard
+        current={current}
+        history={history}
+      />
+    </Grommet>
   );
 };
-
-const Chart = () => {
-  const [range, setRange] = useState([0, 24]);
-  const onChange = (values) => {
-    setRange(values);
-  };
-  return (
-    <Box margin='small' align='center' justify='center'>
-      <Box fill='horizontal' align='center' justify='center'>
-        <DataChart
-          size={{ width: 'fill' }}
-          gap='none'
-          pad={{ horizontal: 'medium', vertical: 'small' }}
-          data={[
-            { dt: '2020-07-01T12:23', t1: 12, t2: 13, tp1: 11, tp2: 14 },
-            { dt: '2020-07-01T12:33', t1: 13, t2: 13, tp1: 11, tp2: 14 },
-            { dt: '2020-07-01T12:43', t1: 12, t2: 14, tp1: 11, tp2: 14 },
-            { dt: '2020-07-01T12:53', t1: 12, t2: 13, tp1: 12, tp2: 15 },
-          ]}
-          series={[{
-            property: 'dt',
-            render: (dt) => (
-              <Text margin={{ horizontal: 'xsmall' }}>
-                {dayjs(dt).format('HH:mm:ss')}
-              </Text>
-            ),
-          }, 't1', 't2', 'tp1', 'tp2']}
-          bounds='align'
-          chart={[{
-            property: 't1',
-            type: 'line',
-            thickness: 'hair',
-            color: 'room1',
-          }, {
-            property: 'tp1',
-            type: 'line',
-            thickness: 'xxsmall',
-            color: 'room1',
-            dash: true,
-          }, {
-            property: 't2',
-            type: 'line',
-            thickness: 'hair',
-            color: 'room2',
-          }, {
-            property: 'tp2',
-            type: 'line',
-            thickness: 'xxsmall',
-            color: 'room2',
-            dash: true,
-          }]}
-          axis={{
-            x: { property: 'dt' },
-            y: {
-              property: 't1',
-              ganularity: 'fine',
-            },
-          }}
-          guide={{
-            x: { granularity: 'fine' },
-            y: { granularity: 'fine' },
-          }}
-          detail
-        />
-      </Box>
-      <Box fill='horizontal' pad={{ left: 'small', right: 'small' }}>
-        <RangeSelector
-          direction='horizontal'
-          min={0}
-          max={24}
-          values={range}
-          onChange={onChange}
-        />
-      </Box>
-    </Box>
-  );
-};
-
-const Entry = ({ label, children }) => (
-  <Box direction='row' margin='xxsmall' gap='xxsmall'>
-    <Box width='2.5em' align='end'>{label}:</Box>
-    <Box width='2.5em' align='start'>{children}</Box>
-  </Box>
-);
-
-const Page = () => {
-  // const [showSidebar, setShowSidebar] = useState(false);
-  const size = React.useContext(ResponsiveContext);
-  const isSmall = size === 'small' || size === 'xsmall';
-  const currSensor = (
-    <Box fill='horizontal' direction='row' justify='center'>
-      <Room
-        id='room1'
-        nid='1'
-        title='Bedroom'
-      />
-      <Room
-        id='room2'
-        nid='2'
-        title='Living Room'
-      />
-      <Room
-        id='env'
-        nid='0'
-        title='Environment'
-      />
-    </Box>
-  );
-  const histSensor = (
-    <Chart />
-  );
-  const currControl = (
-    <Box width={isSmall ? '100%' : '260px'} direction='row' wrap={!isSmall} justify='center' margin={isSmall ? { top: '-2px' } : { right: 'small' }} gap='none'>
-      <Card width={isSmall ? '120px' : '100%'} background='light' margin='small' flex={isSmall ? false : 'grow'}>
-        <CardHeader pad='small' justify='center' background='dark'>F State</CardHeader>
-        <CardBody pad='small' justify='center' margin={{ bottom: '-10px' }} flex>
-          <Box align='center'>
-            <Text weight='bold' color='dark' margin='xsmall' style={{ marginBottom: 0 }}>NORMAL</Text>
-            <Text color='dark' margin='xxsmall'>04:24</Text>
-            <Text weight='bold' color='warm' margin='xxsmall'>+0.3&#8451;</Text>
-          </Box>
-        </CardBody>
-        <CardFooter gap='none' direction='row' backgrund='light' justify='evenly'>
-          <IconButton margin={isSmall && { top: '-7px', bottom: '-7px', left: '-5px', right: '-5px' }} icon={<Subtract color='cold' />} hoverIndicator />
-          <IconButton margin={isSmall && { top: '-7px', bottom: '-7px', left: '-5px', right: '-5px' }} icon={<Radial color='black' />} hoverIndicator />
-          <IconButton margin={isSmall && { top: '-7px', bottom: '-7px', left: '-5px', right: '-5px' }} icon={<Add color='warm' />} hoverIndicator />
-        </CardFooter>
-      </Card>
-      <Card width={isSmall ? '120px' : '100%'} height={{ max: isSmall ? '165px' : undefined }} background='light' margin='small' flex='grow'>
-        <CardHeader pad='small' justify='center' background='dark'>F/C/H0 output</CardHeader>
-        <CardBody direction={isSmall ? 'column' : 'row'} pad='small' wrap justify={isSmall ? 'start' : 'evenly'} overflow={isSmall ? 'scroll' : undefined}>
-          <Entry label='acm'>+2</Entry>
-          <Entry label='acp'>0.12</Entry>
-          <Entry label='reg1'>0.12</Entry>
-          <Entry label='reg2'>0.12</Entry>
-          <Entry label='fan'>1</Entry>
-          <Entry label='win'>0.3</Entry>
-          <Entry label='cur'>1</Entry>
-          <Entry label='w0'>1.0</Entry>
-          <Entry label='w1'>1.0</Entry>
-          <Entry label='w2'>1.0</Entry>
-          <Entry label='curbl'>1</Entry>
-          <Entry label='curbu'>1</Entry>
-          <Entry label='f012'>0.32</Entry>
-          <Entry label='f012bl'>1</Entry>
-          <Entry label='f012bu'>1</Entry>
-          <Entry label='ac1'>+0.32</Entry>
-          <Entry label='ac2'>-0.32</Entry>
-          <Entry label='t1m0'>-30</Entry>
-          <Entry label='Wsun'>1502</Entry>
-          <Entry label='qest'>15.3</Entry>
-          <Entry label='cest'>14.3</Entry>
-          <Entry label='t0d'>14.3</Entry>
-          <Entry label='tsr'>1.0</Entry>
-          <Entry label='tss'>1.0</Entry>
-        </CardBody>
-      </Card>
-    </Box>
-  );
-  let obj;
-  if (isSmall) {
-    obj = (
-      <Box>
-        {currSensor}
-        {currControl}
-        {histSensor}
-      </Box>
-    );
-  } else {
-    obj = (
-      <Box direction='row' gap='small'>
-        <Box width='700px' flex>
-          {currSensor}
-          {histSensor}
-        </Box>
-        {currControl}
-      </Box>
-    );
-  }
-  return obj;
-}
-
-const App = () => (
-  <Grommet theme={theme} full>
-    <Page />
-  </Grommet>
-);
 
 export default App;
---
+++ frontend/src/Dashboard.js
@@ -0,0 +1,287 @@
+import _ from 'lodash';
+import React, { useState } from 'react';
+import {
+  Box,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  DataChart,
+  RangeSelector,
+  ResponsiveContext,
+  Text,
+} from 'grommet';
+import { IconButton } from 'grommet-controls';
+import { Add, Radial, Subtract } from 'grommet-icons';
+import GaugeChart from 'react-gauge-chart';
+import dayjs from 'dayjs';
+import numeral from 'numeral';
+
+const fmt = (v, f) => (v === undefined || isNaN(v)) ? '--' : numeral(v).format(f);
+
+const Room = (props) => {
+  const size = React.useContext(ResponsiveContext);
+  const isSmall = size === 'small' || size === 'xsmall';
+  const { id, nid, title, data, ts } = props;
+  const lb = 19.0, ub = 33.0;
+  const t = _.get(data, 't');
+  const tp = _.get(data, 'tp');
+  let tpct = (t - lb) / (ub - lb);
+  if (tpct < -0.13) {
+    tpct = -0.13;
+  } else if (tpct > 1.13) {
+    tpct = 1.13;
+  }
+  let h = _.get(data, 'h');
+  const hpct = h / 100;
+  const mkArcs = (v) => {
+    if (isNaN(v)) return [0, 1, 0];
+    let pl = (v - lb) / (ub - lb) - 0.05;
+    let pr = (v - lb) / (ub - lb) + 0.05;
+    if (pl < 0) pl = 0; else if (pl > 1) pl = 1;
+    if (pr < 0) pr = 0; else if (pr > 1) pr = 1;
+    return [pl, pr - pl, 1 - pr];
+  };
+  return (
+    <Card width={{ min: '100px' }} background='light' margin='small' flex>
+      <CardHeader pad='small' justify='center' background={id}>{title}</CardHeader>
+      <CardBody pad={{ top: 'small' }} flex='grow' align='center'>
+        <GaugeChart id={'gauge-' + id + '-t'}
+          colors={['#8cb964', '#fae500', '#ca4951']}
+          arcsLength={mkArcs(nid !== '0' ? tp : ts)}
+          arcWidth={0.5}
+          percent={tpct}
+          hideText
+          animate={false}
+        />
+        <Box>
+          <Text weight='bold'>t<sub>{nid}</sub>: {fmt(t, '0.00')}&#8451;</Text>
+        </Box>
+        <Box flex='grow'>
+          {nid !== '0' ? (
+            <Text>t<sub>p{nid}</sub>: {fmt(tp, '0.00')}&#8451;</Text>
+          ) : (
+            <Box direction='row' gap='small'>
+              <Text>{fmt(_.get(data, 'uvi'), '0.00')}</Text>
+              <Text>{fmt(_.get(data, 'wind'), '0.00')}m/s</Text>
+            </Box>
+          )}
+        </Box>
+        {!isSmall && (
+          <GaugeChart id={'gauge-' + id + '-h'}
+            colors={['#ca4951', '#fae500', '#8cb964']}
+            arcsLength={nid === '1' ? [0.45, 0.40, 0.15] : [0.3, 0.55, 0.15]}
+            arcWidth={0.3}
+            percent={hpct}
+            hideText
+            animate={false}
+          />
+        )}
+        <Box>
+          <Text weight='bold'>h<sub>{nid}</sub>: {fmt(_.get(data, 'h'), '0.00')}%</Text>
+        </Box>
+      </CardBody>
+    </Card>
+  );
+};
+
+const Chart = ({ data }) => {
+  const [range, setRange] = useState([0, 24]);
+  const onChange = (values) => {
+    setRange(values);
+  };
+  return (
+    <Box margin='small' align='center' justify='center'>
+      <Box fill='horizontal' align='center' justify='center'>
+        <DataChart
+          size={{ width: 'fill' }}
+          gap='none'
+          pad={{ horizontal: 'medium', vertical: 'small' }}
+          data={Array.isArray(data) ? data : []}
+          series={[{
+            property: 'dt',
+            render: (dt) => (
+              <Text margin={{ horizontal: 'xsmall' }}>
+                {dayjs(dt).format('HH:mm:ss')}
+              </Text>
+            ),
+          }, 't1', 't2', 'tp1', 'tp2']}
+          bounds='align'
+          chart={[{
+            property: 't1',
+            type: 'line',
+            thickness: 'hair',
+            color: 'room1',
+          }, {
+            property: 'tp1',
+            type: 'line',
+            thickness: 'xxsmall',
+            color: 'room1',
+            dash: true,
+          }, {
+            property: 't2',
+            type: 'line',
+            thickness: 'hair',
+            color: 'room2',
+          }, {
+            property: 'tp2',
+            type: 'line',
+            thickness: 'xxsmall',
+            color: 'room2',
+            dash: true,
+          }]}
+          axis={{
+            x: { property: 'dt' },
+            y: {
+              property: 't1',
+              ganularity: 'fine',
+            },
+          }}
+          guide={{
+            x: { granularity: 'fine' },
+            y: { granularity: 'fine' },
+          }}
+          detail
+        />
+      </Box>
+      <Box fill='horizontal' pad={{ left: 'small', right: 'small' }}>
+        <RangeSelector
+          direction='horizontal'
+          min={0}
+          max={24}
+          values={range}
+          onChange={onChange}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+const Entry = ({ data, label, format }) => (
+  <Box direction='row' margin='xxsmall' gap='xxsmall'>
+    <Box width='2.5em' align='end'>{label}:</Box>
+    <Box width='2.5em' align='start'>{fmt(_.get(data, label), format || '0.00')}</Box>
+  </Box>
+);
+
+const Dashboard = ({ history, current, setOffset }) => {
+  const size = React.useContext(ResponsiveContext);
+  const isSmall = size === 'small' || size === 'xsmall';
+  const currSensor = (
+    <Box fill='horizontal' direction='row' justify='center'>
+      <Room
+        id='room1'
+        nid='1'
+        title='Bedroom'
+        data={_.get(current, 'r1')}
+      />
+      <Room
+        id='room2'
+        nid='2'
+        title='Living Room'
+        data={_.get(current, 'r2')}
+      />
+      <Room
+        id='env'
+        nid='0'
+        title='Environment'
+        data={_.get(current, 'r0')}
+        ts={(_.get(current, 'r1.tp') + _.get(current, 'r2.tp')) / 2}
+      />
+    </Box>
+  );
+  const histSensor = (
+    <Chart data={history} />
+  );
+  const state = _.get(current, 'f.state') || '(unknown)';
+  let slept = _.get(current, 'f.slept') / 60;
+  if (slept > 24) slept = 24; if (slept < 0) slept = NaN;
+  slept = `${fmt(Math.floor(slept), 2)}:${fmt((slept - Math.floor(slept)) * 60, 2)}`
+  const other = _.get(current, 'other');
+  const currControl = (
+    <Box width={isSmall ? '100%' : '260px'} direction='row' wrap={!isSmall} justify='center' margin={isSmall ? { top: '-2px' } : { right: 'small' }} gap='none'>
+      <Card width={isSmall ? '120px' : '100%'} background='light' margin='small' flex={isSmall ? false : 'grow'}>
+        <CardHeader pad='small' justify='center' background='dark'>F State</CardHeader>
+        <CardBody pad='small' justify='center' margin={{ bottom: '-10px' }} flex>
+          <Box align='center'>
+            <Text weight='bold' color='dark' margin='xsmall' style={{ marginBottom: 0 }}>{state}</Text>
+            <Text color='dark' margin='xxsmall'>{slept}</Text>
+            <Text weight='bold' color='warm' margin='xxsmall'>{fmt(_.get(current, 'f.offset'), '+0.0')}&#8451;</Text>
+          </Box>
+        </CardBody>
+        <CardFooter gap='none' direction='row' backgrund='light' justify='evenly'>
+          <IconButton
+            margin={isSmall && { top: '-7px', bottom: '-7px', left: '-5px', right: '-5px' }}
+            icon={<Subtract color='cold' />}
+            hoverIndicator
+            onClick={() => { setOffset && setOffset(-1); }}
+          />
+          <IconButton
+            margin={isSmall && { top: '-7px', bottom: '-7px', left: '-5px', right: '-5px' }}
+            icon={<Radial color='black' />}
+            hoverIndicator
+            onClick={() => { setOffset && setOffset(0); }}
+          />
+          <IconButton
+            margin={isSmall && { top: '-7px', bottom: '-7px', left: '-5px', right: '-5px' }}
+            icon={<Add color='warm' />}
+            hoverIndicator
+            onClick={() => { setOffset && setOffset(+1); }}
+          />
+        </CardFooter>
+      </Card>
+      <Card width={isSmall ? '120px' : '100%'} height={{ max: isSmall ? '165px' : undefined }} background='light' margin='small' flex='grow'>
+        <CardHeader pad='small' justify='center' background='dark'>F/C/H0 output</CardHeader>
+        <CardBody direction={isSmall ? 'column' : 'row'} pad='small' wrap justify={isSmall ? 'start' : 'evenly'} overflow={isSmall ? 'scroll' : undefined}>
+          <Entry data={other} label='acm' format='+0.00' />
+          <Entry data={other} label='acp' format='+0.00' />
+          <Entry data={other} label='reg1' />
+          <Entry data={other} label='reg2' />
+          <Entry data={other} label='fan' format='0' />
+          <Entry data={other} label='win' />
+          <Entry data={other} label='cur' />
+          <Entry data={other} label='w0' format='0.0' />
+          <Entry data={other} label='w1' format='0.0' />
+          <Entry data={other} label='w2' format='0.0' />
+          <Entry data={other} label='curbl' />
+          <Entry data={other} label='curbu' />
+          <Entry data={other} label='f012' />
+          <Entry data={other} label='f012bl' />
+          <Entry data={other} label='f012bu' />
+          <Entry data={other} label='ac1' format='+0.00' />
+          <Entry data={other} label='ac2' format='+0.00' />
+          <Entry data={other} label='t1m0' format='+0.00' />
+          <Entry data={other} label='Wsun' />
+          <Entry data={other} label='qest' />
+          <Entry data={other} label='cest' />
+          <Entry data={other} label='t0d' />
+          <Entry data={other} label='tsr' format='+0.0' />
+          <Entry data={other} label='tss' format='+0.0' />
+        </CardBody>
+      </Card>
+    </Box>
+  );
+  let obj;
+  if (isSmall) {
+    obj = (
+      <Box>
+        {currSensor}
+        {currControl}
+        {histSensor}
+      </Box>
+    );
+  } else {
+    obj = (
+      <Box direction='row' gap='small'>
+        <Box width='700px' flex>
+          {currSensor}
+          {histSensor}
+        </Box>
+        {currControl}
+      </Box>
+    );
+  }
+  return obj;
+}
+
+export default Dashboard;
